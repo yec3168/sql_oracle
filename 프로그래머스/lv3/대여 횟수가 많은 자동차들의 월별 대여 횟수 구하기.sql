426b89216de291636471ac804800ff3304868790
yec3168 <yec3168@naver.com>
2024-07-26 16:35:51 +0900

    대여 횟수가 많은 자동차들의 월별 대여 횟수 구허기 (수정)

--- 프로그래머스/lv3/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/lv3/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,11 +1,25 @@
-select month, h.car_id, count(h.car_id) as RECORDS
-from (select car_id, to_number(to_char(start_date, 'fmMM')) as month
-      from car_rental_company_rental_history
-      group by car_id,to_number(to_char(start_date, 'fmMM'))
-      having  to_number(to_char(start_date, 'fmMM')) >= 8 and to_number(to_char(start_date, 'fmMM')) <= 10 and count(car_id) >=5
-     ) c 
-     inner join car_rental_company_rental_history h 
-     on c.car_id = h.car_id
-group by month, h.car_id
-having count(h.car_id)>0
+select extract(month from start_date) as MONTH, car_id, count(distinct history_id) as RECORDS
+from(select *
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+    where car_id in (select car_id
+                    from car_rental_company_rental_history
+                    where to_number(to_char(start_date, 'fmMM')) >= 8 and to_number(to_char(start_date, 'fmMM')) <= 10
+                    group by car_id
+                    having count(DISTINCT HISTORY_ID) >4))
+where to_number(to_char(start_date, 'fmMM')) >= 8 and to_number(to_char(start_date, 'fmMM')) <= 10
+group by extract(month from start_date), car_id
 order by 1, 2 desc
+
+
+
+-- select month, h.car_id, count(h.car_id) as RECORDS
+-- from (select car_id, to_number(to_char(start_date, 'fmMM')) as month
+--       from car_rental_company_rental_history
+--       group by car_id,to_number(to_char(start_date, 'fmMM'))
+--       having  to_number(to_char(start_date, 'fmMM')) >= 8 and to_number(to_char(start_date, 'fmMM')) <= 10 and count(car_id) >=5
+--      ) c 
+--      inner join car_rental_company_rental_history h 
+--      on c.car_id = h.car_id
+-- group by month, h.car_id
+-- having count(h.car_id)>0
+-- order by 1, 2 desc
