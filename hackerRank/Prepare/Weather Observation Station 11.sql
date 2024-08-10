2c6e35e6883142021c35d5d7ba96028b3a2b1432
yec3168 <yec3168@naver.com>
2024-08-10 17:37:57 +0900

    Weather Observation Station 11

---
+++ hackerRank/Prepare/Weather Observation Station 11.sql
@@ -0,0 +1,7 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select distinct city
+from station
+where not  regexp_like(upper(city), '^[AEIOU]') or not regexp_like(upper(city), '[AEIOU]$');
