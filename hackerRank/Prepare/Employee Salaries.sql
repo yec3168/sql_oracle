dde3d7868e25d0b3414b4131827799e7d6d70d58
yec3168 <yec3168@naver.com>
2024-08-11 15:32:29 +0900

    Employee Salaries

---
+++ hackerRank/Prepare/Employee Salaries.sql
@@ -0,0 +1,9 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select name
+from employee
+where months < 10 and salary >= 2000
+order by employee_id;
