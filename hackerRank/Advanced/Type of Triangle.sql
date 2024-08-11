21b9ed5f36b5d2dbdb01c495c26442ea7abf6b7b
yec3168 <yec3168@naver.com>
2024-08-11 15:31:41 +0900

    Triangle 수정

--- hackerRank/Advanced/Type of Triangle.sql
+++ hackerRank/Advanced/Type of Triangle.sql
@@ -3,7 +3,7 @@ Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
 */
 
-select (case   when  A+B < C or A+C < B or B+C < A then 'Not A Triangle'
+select (case   when  A+B <= C or A+C <= B or B+C <= A then 'Not A Triangle'
                     when A = B and B =C then 'Equilateral' 
                     when 
         A = B and B != C or
