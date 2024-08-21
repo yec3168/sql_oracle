4d8e28aa68b72ef3bb48597163e1a1d22354de5c
yec3168 <yec3168@naver.com>
2024-08-21 19:28:54 +0900

    Weather Observation Station 13

---
+++ hackerRank/Aggregation/Weather Observation Station 13.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select sum(lat_n)
+from station
+where lat_n between 38.7880 and 137.2345;
