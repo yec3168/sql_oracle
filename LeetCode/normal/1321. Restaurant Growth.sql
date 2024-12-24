a04f8647a777b6c48d9267d9e238e048acfc23f7
yec3168 <yec3168@naver.com>
2024-12-24 09:35:16 +0900

    [REFACTOR] 1321. Restaurant Growth

--- LeetCode/normal/1321. Restaurant Growth.sql
+++ LeetCode/normal/1321. Restaurant Growth.sql
@@ -1,6 +1,14 @@
 /* Write your PL/SQL query statement below */
-select visited_on+6
-from customer
-group by visited_on    
-having visited_on + 6 <= max(visited_on)
- 
+select to_char(visited_on+6, 'YYYY-MM-DD') as visited_on,
+            -- sum(amount) over( order by visited_on ) 
+            (select sum(amount)
+             from customer c2
+             where c2.visited_on between c1.visited_on and c1.visited_on+6 ) as amount,
+            (select round(sum(amount)/7, 2)
+             from customer c2
+             where c2.visited_on between c1.visited_on and c1.visited_on+6 ) as average_amount 
+from customer c1
+where visited_on + 6 <= (select max(visited_on)
+                        from customer)
+group by visited_on
+order by 1;
