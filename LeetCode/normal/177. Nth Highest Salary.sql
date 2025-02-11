09bd94f812ad3e52d082372037be274b2d5c4c6a
yec3168 <yec3168@naver.com>
2025-02-12 08:58:06 +0900

    177. Nth Highest Salary

--- LeetCode/normal/177. Nth Highest Salary.sql
+++ LeetCode/normal/177. Nth Highest Salary.sql
@@ -2,17 +2,12 @@
 CREATE FUNCTION getNthHighestSalary(num IN NUMBER) RETURN NUMBER IS
 result NUMBER;
 BEGIN
-    SELECT 
-         SALARY INTO result
-      FROM
-        (SELECT
-            SALARY, 
-            DENSE_RANK() OVER (ORDER BY SALARY DESC) AS RANK
-         FROM
-            EMPLOYEE)
-      WHERE
-        RANK = num
-      AND
-        ROWNUM = 1;  
+    SELECT SALARY INTO RESULT
+    FROM (
+            SELECT salary,
+                    DENSE_RANK() OVER(ORDER BY SALARY DESC) AS RANK
+            FROM EMPLOYEE
+            )
+    WHERE RANK = NUM AND ROWNUM = 1;
     RETURN result;
 END;
